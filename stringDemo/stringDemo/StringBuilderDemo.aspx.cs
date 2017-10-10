42c93e143e2eff6db452172a94cdd51604b098c5
Strhoads <31014934+Strhoads@users.noreply.github.com>
2017-10-10 14:00:24 -0400

    Difference between string and StringBuilder class

---
+++ stringDemo/stringDemo/StringBuilderDemo.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace stringDemo
+{
+    public partial class StringBuilderDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] text = myTextBox.Text.Split(',');
+            for (int i = 0; i < text.Length; i++)
+            {
+                sb.Append(text[i]);
+                sb.Append(" ");
+                sb.Append("<br />");
+            }
+            resultLabel.Text = sb.ToString();
+        }
+    }
+}
