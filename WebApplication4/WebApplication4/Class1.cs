26c9c9021fda9eae99acd89c7975fe8919f006c9
Strhoads <31014934+Strhoads@users.noreply.github.com>
2017-10-20 13:45:23 -0400

    CS Final

---
+++ WebApplication4/WebApplication4/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace WebApplication4
+{
+    public void ImportDataFromExcel(string excelFilePath)
+    {
+        string ssqlTable = "Table1";
+        string myExcelDataQuery = "Select person,address,city,state,zip from [Sheet1$]";
+        try
+        {
+            string sexcelconnectionstring = @"provider=microsoft.jet.oledb.4.0;data source=" + excelFilePath + ";extended properties=" + "\"excel8.0;hdr=yes;\"";
+            string ssqlconnectionstring = "Data Source=SAYYED;Initial Catalog=SyncDB;Integrated Security=True";
+
+            string sclearsql = "delete from " + ssqlTable;
+            SqlConnection sqlConn = new SqlConnection(ssqlconnectionstring);
+            SqlCommand sqlCmd = new SqlCommand(sclearsql, sqlConn);
+            sqlConn.Open();
+            sqlCmd.ExecuteNonQuery();
+            sqlConn.Close();
+            OleDbConnection oleDbConn = new OleDbConnection(sexcelconnectionstring);
+            OleDbCommand oleDbCmd = new OleDbCommand(myExcelDataQuery, oleDbConn);
+            oleDbConn.Open();
+            OleDbDataReader dr = oleDbCmd.ExecuteReader();
+            SqlBulkCopy bulkCopy = new SqlBulkCopy(ssqlconnectionstring);
+            bulkCopy.DestinationTableName = ssqlTable;
+            while (dr.Read())
+            {
+                bulkCopy.WriteToServer(dr);
+            }
+            dr.Close();
+            oleDbConn.Close();
+            Label1.Text = "File imported into sql server.";
+
+        }
+        catch (Exception ex)
+        {
+            //handle 
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+            <br />
+            <asp:Button ID="submitButton" runat="server" OnClick="submitButton_Click" Text="Submit" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
